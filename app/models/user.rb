56d7acd5a83b336412180ae303277bf6f54658b3
Shannon Nolan <shannonnolan17@gmail.com>
2017-10-14 10:49:58 -0700

    Create user login and registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  validates :first_name, :email, :password, presence: true
+  validates :email, uniqueness: true
   has_many :reviews
   has_many :organizations, :through => :reviews
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
